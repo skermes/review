8c716b635f70698f7fabe369da55c17747c20e42
Sean Kermes <skermes@gmail.com>
2011-10-10 19:33:07 -0400

    Turning off --word-diff.
    There are some issues with whitespace and --word-diff.
    Turning it off for now to focus on other stuff.

--- review.rb
+++ review.rb
@@ -12,8 +12,25 @@ end
 
 get '/review/:branch' do
 	#git('fetch')
-	diff = git("diff -U10 --ignore-space-change --word-diff master..#{params[:branch]}")
-	@snippets = parse(diff)
+	diff = git("diff -U10 --ignore-space-change master..#{params[:branch]}")
+	@snippets = []
+	diff.each_line do |line|
+		if line.start_with?('diff')
+			@snippets << { :content => line, :type => :header }
+		elsif line.start_with?('index') or
+			  line.start_with?('---') or
+			  line.start_with?('+++')
+			@snippets[-1][:content] += line
+		elsif line.start_with?('@@')
+			@snippets << { :content => line, :type => :separator }
+		elsif line.start_with?('+')
+			@snippets << { :content => line, :type => :addition }
+		elsif line.start_with?('-')
+			@snippets << { :content => line, :type => :subtraction }
+		else
+			@snippets << { :content => line, :type => :raw }
+		end
+	end
 	@title = params[:branch]
 	haml :review
 end
@@ -31,22 +48,3 @@ get '/:id' do
 	@title = params[:id]
 	haml :reviewed
 end
-
-#@snippets = []
-#diff.each_line do |line|
-#	if line.start_with?('diff')
-#		@snippets << { :content => line, :type => :header }
-#	elsif line.start_with?('index') or
-#		  line.start_with?('---') or
-#		  line.start_with?('+++')
-#		@snippets[-1][:content] += line
-#	elsif line.start_with?('@@')
-#		@snippets << { :content => line, :type => :separator }
-#	elsif line.start_with?('+')
-#		@snippets << { :content => line, :type => :addition }
-#	elsif line.start_with?('-')
-#		@snippets << { :content => line, :type => :subtraction }
-#	else
-#		@snippets << { :content => line, :type => :raw }
-#	end
-#end
