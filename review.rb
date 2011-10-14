5fe7877455be51379e253b538d48aed59acf4721
Sean Kermes <skermes@gmail.com>
2011-10-13 20:55:38 -0400

    Moving all the diff parsing into one file.

--- review.rb
+++ review.rb
@@ -20,24 +20,7 @@ def review(from_branch, to_branch)
 	end
 	branch_prefix = REMOTE_BRANCHES ? REMOTE_NAME + '/' : ''
 	diff = git("diff -U10 --ignore-space-change #{branch_prefix}#{from_branch}...#{branch_prefix}#{to_branch}")
-	@snippets = []
-	diff.each_line do |line|
-		if line.start_with?('diff')
-			@snippets << { :content => line, :type => :header }
-		elsif line.start_with?('index') or
-			  line.start_with?('---') or
-			  line.start_with?('+++')
-			@snippets[-1][:content] += line
-		elsif line.start_with?('@@')
-			@snippets << { :content => line, :type => :separator }
-		elsif line.start_with?('+')
-			@snippets << { :content => line, :type => :addition }
-		elsif line.start_with?('-')
-			@snippets << { :content => line, :type => :subtraction }
-		else
-			@snippets << { :content => line, :type => :raw }
-		end
-	end
+	@snippets = DiffParsing.parse(:unified, diff)
 	@title = to_branch
 	haml :review, :escape_html => true
 end
