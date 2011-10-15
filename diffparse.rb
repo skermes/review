f07f1f89c1eef0fb253d76982c101108f19654d3
Sean Kermes <skermes@gmail.com>
2011-10-15 10:26:22 -0400

    De-tabifying my whitespace.

--- diffparse.rb
+++ diffparse.rb
@@ -1,91 +1,91 @@
 module DiffParsing
-	def DiffParsing.parse(format, diff)
-		if format == :unified
-			parse_unified_diff(diff)
-		elsif format == :word
-			parse_word_diff(diff)
-		else
-			{ :content => diff, :type => raw }
-		end
-	end
+    def DiffParsing.parse(format, diff)
+        if format == :unified
+            parse_unified_diff(diff)
+        elsif format == :word
+            parse_word_diff(diff)
+        else
+            { :content => diff, :type => raw }
+        end
+    end
 
-	def DiffParsing.parse_word_diff(diff)
-		snippets = []
-	
-		in_addition = false
-		in_subtraction = false
-		diff.each_line do |line|
-			if line.start_with? 'diff'
-				snippets << { :content => line, :type => :header }
-			elsif line.start_with? 'index' or
-				  line.start_with? '---' or
-				  line.start_with? '+++'
-				snippets[-1][:content] += line
-			elsif line.start_with? '@@'
-				snippets << { :content => line, :type => :separator }
-			else
-				if snippets[-1][:type] == :separator
-					snippets << { :content => '', :type => :raw }
-				end
-				skip = false
-				line.chars().zip(line[1..-1].chars()).each do |char, next_char|
-					if skip
-						skip = false
-					elsif char == '{' and
-					   next_char == '+' and
-					   not in_addition and
-					   not in_subtraction
-						in_addition = true
-						snippets << { :content => char, :type => :addition }
-					elsif char == '+' and
-						  next_char == '}' and
-						  in_addition
-						in_addition = false
-						snippets[-1][:content] += '+}'
-						snippets << { :content => '', :type => :raw }
-						skip = true
-					elsif char == '[' and
-						  next_char == '-' and
-						  not in_addition and
-						  not in_subtraction
-						in_subtraction = true
-						snippets << { :content => char, :type => :subtraction }
-					elsif char == '-' and
-						  next_char == ']' and
-						  in_subtraction
-						in_subtraction = false
-						snippets[-1][:content] += '-]'
-						snippets << { :content => '', :type => :raw }
-						skip = true
-					else
-						snippets[-1][:content] += char
-					end
-				end
-			end
-		end
+    def DiffParsing.parse_word_diff(diff)
+        snippets = []
+    
+        in_addition = false
+        in_subtraction = false
+        diff.each_line do |line|
+            if line.start_with? 'diff'
+                snippets << { :content => line, :type => :header }
+            elsif line.start_with? 'index' or
+                  line.start_with? '---' or
+                  line.start_with? '+++'
+                snippets[-1][:content] += line
+            elsif line.start_with? '@@'
+                snippets << { :content => line, :type => :separator }
+            else
+                if snippets[-1][:type] == :separator
+                    snippets << { :content => '', :type => :raw }
+                end
+                skip = false
+                line.chars().zip(line[1..-1].chars()).each do |char, next_char|
+                    if skip
+                        skip = false
+                    elsif char == '{' and
+                       next_char == '+' and
+                       not in_addition and
+                       not in_subtraction
+                        in_addition = true
+                        snippets << { :content => char, :type => :addition }
+                    elsif char == '+' and
+                          next_char == '}' and
+                          in_addition
+                        in_addition = false
+                        snippets[-1][:content] += '+}'
+                        snippets << { :content => '', :type => :raw }
+                        skip = true
+                    elsif char == '[' and
+                          next_char == '-' and
+                          not in_addition and
+                          not in_subtraction
+                        in_subtraction = true
+                        snippets << { :content => char, :type => :subtraction }
+                    elsif char == '-' and
+                          next_char == ']' and
+                          in_subtraction
+                        in_subtraction = false
+                        snippets[-1][:content] += '-]'
+                        snippets << { :content => '', :type => :raw }
+                        skip = true
+                    else
+                        snippets[-1][:content] += char
+                    end
+                end
+            end
+        end
 
-		snippets
-	end
+        snippets
+    end
 
-	def DiffParsing.parse_unified_diff(diff)
-		snippets = []
-		diff.each_line do |line|
-			if line.start_with?('diff')
-				snippets << { :content => line, :type => :header }
-			elsif line.start_with?('index') or
-				  line.start_with?('---') or
-				  line.start_with?('+++')
-				snippets[-1][:content] += line
-			elsif line.start_with?('@@')
-				snippets << { :content => line, :type => :separator }
-			elsif line.start_with?('+')
-				snippets << { :content => line, :type => :addition }
-			elsif line.start_with?('-')
-				snippets << { :content => line, :type => :subtraction }
-			else
-				snippets << { :content => line, :type => :raw }
-			end
-		end
-		snippets
-	end
+    def DiffParsing.parse_unified_diff(diff)
+        snippets = []
+        diff.each_line do |line|
+            if line.start_with?('diff')
+                snippets << { :content => line, :type => :header }
+            elsif line.start_with?('index') or
+                  line.start_with?('---') or
+                  line.start_with?('+++')
+                snippets[-1][:content] += line
+            elsif line.start_with?('@@')
+                snippets << { :content => line, :type => :separator }
+            elsif line.start_with?('+')
+                snippets << { :content => line, :type => :addition }
+            elsif line.start_with?('-')
+                snippets << { :content => line, :type => :subtraction }
+            else
+                snippets << { :content => line, :type => :raw }
+            end
+        end
+        snippets
+    end
 end
--- review.rb
+++ review.rb
@@ -11,38 +11,38 @@ REMOTE_NAME = 'origin'
 set :haml, :format => :html5, :ugly => true
 
 def git(cmd)
-	%x[cd #{REPO} && git #{cmd}]
+    %x[cd #{REPO} && git #{cmd}]
 end
 
 def review(from_branch, to_branch)
-	if REMOTE_BRANCHES
-		git('fetch')
-	end
-	branch_prefix = REMOTE_BRANCHES ? REMOTE_NAME + '/' : ''
-	diff = git("diff -U10 --ignore-space-change #{branch_prefix}#{from_branch}...#{branch_prefix}#{to_branch}")
-	@snippets = DiffParsing.parse(:unified, diff)
-	@title = to_branch
-	haml :review, :escape_html => true
+    if REMOTE_BRANCHES
+        git('fetch')
+    end
+    branch_prefix = REMOTE_BRANCHES ? REMOTE_NAME + '/' : ''
+    diff = git("diff -U10 --ignore-space-change #{branch_prefix}#{from_branch}...#{branch_prefix}#{to_branch}")
+    @snippets = DiffParsing.parse(:unified, diff)
+    @title = to_branch
+    haml :review, :escape_html => true
 end
 
 get '/review/:branch' do
-	review('master', params[:branch])
+    review('master', params[:branch])
 end
 
 get '/review/:from/to/:to' do
-	review(params[:from], params[:to])
+    review(params[:from], params[:to])
 end
 
 post '/:id' do
-	review = File.new("#{params[:id]}.diffbody", 'w')
-	review.write(params[:diff].tr("\r", ''))
-	review.close
+    review = File.new("#{params[:id]}.diffbody", 'w')
+    review.write(params[:diff].tr("\r", ''))
+    review.close
 end
 
 get '/:id' do
-	reviewfile = File.open("#{params[:id]}.diffbody", 'r')
-	@review = reviewfile.read
-	reviewfile.close
-	@title = params[:id]
-	haml :reviewed
+    reviewfile = File.open("#{params[:id]}.diffbody", 'r')
+    @review = reviewfile.read
+    reviewfile.close
+    @title = params[:id]
+    haml :reviewed
 end
