3e3a145e0735a0b5a0f25388d61fba64a63714ed
scott <scott.dugas@gmail.com>
2011-11-05 12:44:43 -0400

    Added summary block that has commit hash & message
    This way, when looking at a diff with notes, you'll know exactly which
    commits it was looking at

--- review.rb
+++ review.rb
@@ -41,6 +41,8 @@ def review(from_branch, to_branch)
     end
     branch_prefix = REMOTE_BRANCHES ? REMOTE_NAME + '/' : ''
     diff = git("diff -U10 --no-color --ignore-space-change #{branch_prefix}#{from_branch}...#{branch_prefix}#{to_branch}")
+    @branch_text = git("log --no-color -n1 --pretty=medium #{branch_prefix}#{to_branch}")
+    @parent_text = git("log --no-color -n1 --pretty=medium #{branch_prefix}#{from_branch}")
     @snippets = DiffParsing.parse(:unified, diff)
     @branch = to_branch
     @parent = from_branch
